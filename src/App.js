0eb028d2a4123273c1f7a2a4640c87fc15ef1464
Md Nayeem <nayeemmd229@gmail.com>
2023-11-12 12:55:13 +0600

    Tours added scss,fontawesome,gfort &  nav component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from './components/navbar/Navbar';
+
+/* ✅
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'; 
+ */
+//google font is used via cdn link in index html
+
+
+
+
+//Google fonts component
+// import styled from 'styled-components';
+// const StyledText = styled.p`font-family: 'FontStyle', sans-serif;`;
+/* const StyledText = styled.p`font-family: 'FontStyle', sans-serif;`;
+const StyledTexta = styled.h1`font-family: 'Noto Serif Display', serif;`; */
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <StyledTexta>Application</StyledTexta> */}
+      {/* <h1>Application</h1> */}
+      {/* <StyledText>City Tours app.</StyledText> */}
+      {/* <p>City Tours app.</p>  */}
+      {/* <FontAwesomeIcon icon={icon({name: 'coffee', style: 'regular'})}></FontAwesomeIcon> */}
+      {/* <div> */}
+        {/* This is the dynamic application of fontawesome - via Babel Macros */}
+        {/*✅ <FontAwesomeIcon icon={icon({name: 'twitter', style: 'brands'})} /> 
+        <FontAwesomeIcon icon={icon({name: 'user', family:'classic' , style:'solid'})}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={icon({name:'earth-america', style:'solid' })} size='6x' />
+        <FontAwesomeIcon icon={icon({name:'earth-america', style:'solid' })} rotation='90' />
+        <FontAwesomeIcon icon={icon({name:'earth-america', style:'solid' })} beatFade  />
+        <FontAwesomeIcon icon={icon({name:'earth-america', style:'solid' })} spinPulse   />
+        <FontAwesomeIcon icon={icon({name:'earth-america', style:'solid' })} bounce />
+        <FontAwesomeIcon icon={icon({name:'earth-america', style:'solid' })} spin />
+        <FontAwesomeIcon icon={icon({name:'earth-america', style:'solid' })} spinPulse /> ✅ */}
+        {/* <FontAwesomeIcon icon="fa-solid fa-earth-americas" /> ❌ */}
+      {/* </div> */}
+      
+      <Navbar></Navbar>
+
+
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import img from '../../logo.svg';
+
+export default function Navbar() {
+  return (
+      <nav className='navbar'>
+        <img src={img} alt='City Tours'></img>
+        {/* ul.nav-links */}
+        <ul className="nav-links">
+          {/* li>a.nav-link */}
+          <li><a href="/" className="nav-link">Home</a></li>
+          <li><a href="/" className="nav-link">About</a></li>
+          <li><a href="/" className="nav-link active">Tours</a></li>
+        </ul>
+      </nav>
+  )
+}
